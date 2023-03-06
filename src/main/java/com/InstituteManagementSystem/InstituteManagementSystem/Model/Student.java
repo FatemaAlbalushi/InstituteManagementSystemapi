134c4a729b1359ddaf6db5674a59df87cb62ac01
FatemaAlbalushi <fatoomsaid188@gmail.com>
2023-03-06 09:47:48 +0400

    Institute Management System API

---
+++ src/main/java/com/InstituteManagementSystem/InstituteManagementSystem/Model/Student.java
@@ -0,0 +1,22 @@
+package com.InstituteManagementSystem.InstituteManagementSystem.Model;
+
+/**
+ * The Student class represents a student in the Institute Management System.
+ */
+public class Student {
+
+    /**
+     * The ID of the student.
+     */
+    public int id;
+
+    /**
+     * The name of the student.
+     */
+    public String name;
+
+    /**
+     * The email address of the student.
+     */
+    public String email;
+}
